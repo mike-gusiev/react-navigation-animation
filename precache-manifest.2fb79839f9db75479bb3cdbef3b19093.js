6123f93ece453522f108b2b3c9acb9b082510bd5
MikeG <mihail.gusiev@gmail.com>
2020-06-14 20:07:20 +0300

    Updates

---
+++ precache-manifest.2fb79839f9db75479bb3cdbef3b19093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ce955dc4c22ed747f4043ca0a51d50",
+    "url": "/react-navigation-animation/index.html"
+  },
+  {
+    "revision": "30a9635849a8a3943d5d",
+    "url": "/react-navigation-animation/static/css/main.e034e310.chunk.css"
+  },
+  {
+    "revision": "52a39832e71bd7dfb5e4",
+    "url": "/react-navigation-animation/static/js/2.ea32d43b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-navigation-animation/static/js/2.ea32d43b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30a9635849a8a3943d5d",
+    "url": "/react-navigation-animation/static/js/main.7b3e8bbd.chunk.js"
+  },
+  {
+    "revision": "753e43a0c8de7eb01d11",
+    "url": "/react-navigation-animation/static/js/runtime-main.43b76bf9.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-navigation-animation/precache-manifest.2fb79839f9db75479bb3cdbef3b19093.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-navigation-animation/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
